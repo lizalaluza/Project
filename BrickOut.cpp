08b5a2da1f266b367832733b0696a15ba9110326
lizalaluza <70812914+lizalaluza@users.noreply.github.com>
2020-12-12 14:48:13 +0300

    Update BrickOut.cpp

--- BrickOut.cpp
+++ BrickOut.cpp
@@ -16,51 +16,32 @@ int main()
     float sticklx = 160;
     float stickly = 7; 
     float stickx = windowx / 2 - sticklx / 2;
-    float sticky = windowy - (50 + stickly); 
+    float sticky = windowy - (50 + stickly);
+    int gamestate = 1; 
 
-    int array[40];
-    array[0] = {1};
-    array[1] = {1};
-    array[2] = {1};
-    array[3] = {1};
-    array[4] = {1};
-    array[5] = {1};
-    array[6] = {1};
-    array[7] = {1};
-    array[8] = {1};
-    array[9] = {1};
-    array[10] = {1};
-    array[11] = {1};
-    array[12] = {1};
-    array[13] = {1};
-    array[14] = {1};
-    array[15] = {1};
-    array[16] = {1};
-    array[17] = {1};
-    array[18] = {1};
-    array[19] = {1};
-    array[20] = {1};
-    array[21] = {1};
-    array[22] = {1};
-    array[23] = {1};
-    array[24] = {1};
-    array[25] = {1};
-    array[26] = {1};
-    array[27] = {1};
-    array[28] = {1};
-    array[29] = {1};
-    array[30] = {1};
-    array[31] = {1};
-    array[32] = {1};
-    array[33] = {1};
-    array[34] = {1};
-    array[35] = {1};
-    array[36] = {1};
-    array[37] = {1};
-    array[38] = {1};
-    array[39] = {1};
-    array[40] = {1};
+    Brick bricks[40];
+    int k = 1;
+    int j = 0;
+    int m = 0;
+    for (int i = 0; i < 40; i++)
+    {
+        m = k % 2;
+        bricks[i].exist = true;
+        bricks[i].x = 250 - 40 * m + 83 * j;
+        bricks[i].y = 100 + 35 * k;
+        j += 1;
+        if (j >= 10)
+            {
+                j = 0;
+                k += 1;
+            }
+    }
 
+    /*bool array[40];
+    for (int i=0; i < 40; i++)
+    {
+        array[i] = true;
+    }*/
     
     float y = windowy - (50 + stickly + 2 * radius);
     float x = windowx / 2 - radius;
@@ -78,36 +59,27 @@ int main()
     rectangle1.setPosition({ stickx, sticky });
     rectangle1.setFillColor(sf::Color::Red);
 
-    while (array[40] == 1)
+    while (gamestate == 1)
     {
         window.clear();
         shape.setPosition({ x, y });
         rectangle.setPosition({ stickx, sticky });
         window.draw(rectangle);
         window.draw(shape);
-        int k = 0;
-        int j = 0;
-        int m = 0;
         for (int i = 0; i < 40; i++)
         {   
-            m = k % 2;
-            rectangle1.setPosition({ 250 - 40 * m + 83 * j, 100 + 35 * k });
-            if (array[i] == 1)
+            rectangle1.setPosition({ bricks[i].x, bricks[i].y });
+            if (bricks[i].exist)
             {
                 window.draw(rectangle1);    
             }
-
-            j += 1;
-            if (j >= 10)
-            {
-                j = 0;
-                k += 1;
-            }
-
         }
         checkCollision(&x, &vx, &y, &vy, windowx, windowy, radius);
         collisionTablet(stickx, sticky, sticklx, x, y, &vy, radius, DT);
-        brickCollision(x, y, &vx, &vy, radius, DT, array);
+        for (int i = 0; i < 40; i++)
+        {
+            brickCollision(x, y, &vx, &vy, radius, DT, &bricks[i]);
+        }
         circleMove(&x, vx, &y, vy);
         float mousex = sf::Mouse::getPosition(window).x;
 
@@ -121,31 +93,31 @@ int main()
         if (y >= 780)
         {
             window.clear();
-            array[40] = {2};
+            gamestate = 2;
         }
 
         int q = 0;
-        for (int i = 0; i < 41; i++)
+        for (int i = 0; i < 40; i++)
         {
-            if (array[i] == 0)
+            if ( bricks[i].exist == false)
             {
                q += 1;
             }
 
-            else if (q != 40)
+            else
             {
                 break;
             }
 
-            else if (q == 40)
+            if (q == 40)
             {
                 window.clear();
-                array[40] = {3};
+                gamestate = 3;
             }
         }  
     }
 
-    if (array[40] == 2)
+    if (gamestate == 2)
     {
         sf::Texture t;
         t.loadFromFile("lose.jpg");
@@ -167,7 +139,7 @@ int main()
         }
     }
 
-    if (array[40] == 3)
+    if (gamestate == 3)
     {
         sf::Texture t;
         t.loadFromFile("win.jpg");
